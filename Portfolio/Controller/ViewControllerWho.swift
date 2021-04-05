819ec6ada1464c21d0fa2f54e9634728b13e98c9
knyaazev <knyazevilya03@gmail.com>
2021-04-05 15:29:57 +0300

    button hyperlinks added

--- Portfolio/Controller/ViewControllerWho.swift
+++ Portfolio/Controller/ViewControllerWho.swift
@@ -20,4 +20,22 @@ class ViewController: UIViewController {
         
         NameStack.backgroundColor = UIColor(displayP3Red: 0.34509803921568627, green: 0.33725490196078434, blue: 0.8392156862745098, alpha: 0.3)
     }
-}
+    
+    @IBAction func TapGitHub(_ sender: Any) {
+        openUrl(urlStr: "https://github.com")
+    }
+    
+    @IBAction func TapLinkedIn(_ sender: Any) {
+        openUrl(urlStr: "https://www.linkedin.com")
+    }
+    
+    @IBAction func TapTelegram(_ sender: Any) {
+        openUrl(urlStr: "https://web.telegram.org/#/login")
+    }
+    
+    
+    func openUrl(urlStr: String!) {
+        if let url = URL(string:urlStr), !url.absoluteString.isEmpty {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }}
