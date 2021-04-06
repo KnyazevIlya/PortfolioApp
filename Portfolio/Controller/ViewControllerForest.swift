32782b86262b8870a97208b565a78ffb91088f17
knyaazev <knyazevilya03@gmail.com>
2021-04-06 20:15:41 +0300

    Forest screen added

--- Portfolio/Controller/ViewControllerForest.swift
+++ Portfolio/Controller/ViewControllerForest.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewControllerForest: UIViewController {
+    
+    @IBOutlet var atvImg: UIImageView!
+    @IBOutlet var mushroomImg: UIImageView!
+    @IBOutlet var herbImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setImageStyle(image: atvImg)
+        setImageStyle(image: mushroomImg)
+        setImageStyle(image: herbImg)
+    }
+    func setImageStyle(image: UIImageView) {
+        image.layer.cornerRadius = 10
+        image.layer.masksToBounds = true
     }
 }
